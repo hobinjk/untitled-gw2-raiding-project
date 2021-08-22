27d362e314e22a28bd1cc9ab47ab11e5090e6966
James Hobin <hobinjk@gmail.com>
2021-08-22 16:44:35 -0400

    Add boringMechanics to guessRole

--- guessRole.js
+++ guessRole.js
@@ -65,3 +65,46 @@ export default function guessRole(log, player) {
   }
   return `${dType} ${role}`;
 }
+
+export const boringMechanics = {
+  'Got up': true,
+  Res: true,
+  // VG
+  'Green Cast R': true,
+  'Green Cast G': true,
+  'Green Cast B': true,
+  'Invuln Strip': true,
+  // Gorseval
+  // Sabetha
+  // Slothasor
+  // Matthias Gabrel
+  // KC
+  'Rift#': true,
+  // Xera
+  Button1: true,
+  Button2: true,
+  Button3: true,
+  Shield: true,
+  // Cairn
+  // Mursaat Overseer
+  Dispel: true,
+  // Samarog
+  'G.Fix': true,
+  'R.Fix': true,
+  // Deimos
+  Green: true,
+  TP: true,
+  'DMG Debuff': true,
+  // Soulless Horror
+  // Dhuum
+  // Conjured Amalgamate
+  Pulverize: true,
+  // Twin Largos
+  'Ken Aura': true,
+  'Nik Aura': true,
+  // Qadim
+  Claw: true,
+  // Adina
+  // Sabir
+  // Qadim the Peerless
+};
