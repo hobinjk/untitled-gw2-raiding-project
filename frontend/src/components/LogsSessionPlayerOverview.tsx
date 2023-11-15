555f210a8ad8751b260eea27ba2f1adb6885b0e2
James Hobin <hobinjk@gmail.com>
2023-11-14 21:06:38 -0500

    Limit downs/deaths to successful logs if applicable
    Old change that I'm unsure of the rationale for. Might need adjustment

--- frontend/src/components/LogsSessionPlayerOverview.tsx
+++ frontend/src/components/LogsSessionPlayerOverview.tsx
@@ -8,13 +8,16 @@ type IPlayerStats = {
   roles: Array<string>,
   downs: number|string,
   deaths: number|string,
+  downsAll: number|string,
+  deathsAll: number|string,
   scoreDps: number|string,
   scoreSupport: number|string,
   swaps: number,
 }
 
-type IOLogsSessionPlayerOverviewState = {
+type ILogsSessionPlayerOverviewState = {
   loading: boolean,
+  anySuccess: boolean,
   playerStats: {[name: string]: IPlayerStats},
 }
 
@@ -33,7 +36,9 @@ export default function LogsSessionPlayerOverview(props: any) {
           account: player.account,
           roles: [],
           downs: '?',
+          downsAll: '?',
           deaths: '?',
+          deathsAll: '?',
           scoreDps: '?',
           scoreSupport: '?',
           swaps: 0,
@@ -47,8 +52,9 @@ export default function LogsSessionPlayerOverview(props: any) {
     }
   }
 
-  let [appState, setAppState] = useState<IOLogsSessionPlayerOverviewState>({
+  let [appState, setAppState] = useState<ILogsSessionPlayerOverviewState>({
     loading: false,
+    anySuccess: false,
     playerStats: initialPlayerStats,
   });
 
@@ -56,10 +62,14 @@ export default function LogsSessionPlayerOverview(props: any) {
     appState.loading = true;
     setAppState(appState);
     const load = async () => {
+      let anySuccess = false;
       let allMechanicsProms = logs.map(async (logMeta: any) => {
         const logId = logMeta.log_id;
         const res = await API.fetch(`/api/v0/logs/${logId}`);
         const log = await res.json();
+        if (log.success) {
+          anySuccess = true;
+        }
         let nameToAccount: {[name: string]: string} = {};
         for (let player of log.players) {
           nameToAccount[player.name] = player.account;
@@ -88,33 +98,40 @@ export default function LogsSessionPlayerOverview(props: any) {
       let allMechanics: Array<{[name: string]: Array<any>}> = await Promise.all(allMechanicsProms);
       for (const playerName in appState.playerStats) {
         appState.playerStats[playerName].deaths = 0;
+        appState.playerStats[playerName].deathsAll = 0;
         appState.playerStats[playerName].downs = 0;
+        appState.playerStats[playerName].downsAll = 0;
       }
       for (let allMechLog of allMechanics) {
         for (let playerName in allMechLog) {
           for (let occ of allMechLog[playerName]) {
-            if (!occ.success) {
-              continue;
-            }
             if (occ.name === 'Dead') {
               let stats = appState.playerStats[playerName]
-              stats.deaths = (stats.deaths as number) + 1;
+              if (occ.success) {
+                stats.deaths = (stats.deaths as number) + 1;
+              }
+              stats.deathsAll = (stats.deathsAll as number) + 1;
             }
             if (occ.name === 'Downed') {
               let stats = appState.playerStats[playerName]
-              stats.downs = (stats.downs as number) + 1;
+              if (occ.success) {
+                stats.downs = (stats.downs as number) + 1;
+              }
+              stats.downsAll = (stats.downsAll as number) + 1;
             }
           }
         }
       }
       setAppState({
         loading: false,
+        anySuccess,
         playerStats: appState.playerStats,
       });
     };
     load();
   }, [setAppState]);
 
+  let anySuccess = appState.anySuccess;
   let playerStatsList = Object.values(appState.playerStats);
   playerStatsList.sort((a, b) =>
     a.account.toLowerCase().localeCompare(b.account.toLowerCase()));
@@ -155,8 +172,8 @@ export default function LogsSessionPlayerOverview(props: any) {
         let uniqueRoles = Object.keys(uniqueRolesMap);
         return (<tr>
           <td>{playerStat.account}</td>
-          <td>{playerStat.downs}</td>
-          <td>{playerStat.deaths}</td>
+          <td>{anySuccess ? playerStat.downs : playerStat.downsAll}</td>
+          <td>{anySuccess ? playerStat.deaths : playerStat.deathsAll}</td>
           <td>{playerStat.scoreDps}</td>
           <td>{playerStat.scoreSupport}</td>
           <td>{playerStat.swaps}</td>
